9a209fdb05b9eb9b63c341909852dc2673e5832d
rashedrk <rashedrk001@gmail.com>
2023-04-14 22:55:10 +0600

    sing in with google and singout added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import {GoogleAuthProvider, getAuth} from 'firebase/auth'
+import React, { useState } from 'react';
+import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from 'firebase/auth'
 import app from '../../firebase/firebase.init';
 
 const Login = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
+    const [user, setUser] = useState([]);
+
     const handleGoogleAuth = () => {
-        console.log('google is coming');
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            setUser(loggedInUser);
+        })
+        .catch(error => console.log(error))
     }
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser(null);
+                console.log('Sing Out success');
+            })
+    }
+    // console.log(user);
     return (
         <div>
-            <button onClick={handleGoogleAuth}>Google Login</button>
+            {
+                user ? 
+                <div>
+                    <button onClick={handleSignOut}>Sing Out</button>
+                    <h2>Name: {user.displayName}</h2>
+                    <p>Email: {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+                : 
+                <button onClick={handleGoogleAuth}>Google Login</button>
+            }
         </div>
     );
 };
